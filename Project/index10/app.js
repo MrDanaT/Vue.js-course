d13216da964c0f71868f0b9affec9b4708ec696d
Dana Tabatabaie Irani <dana.tabatabaieirani@gmail.com>
2020-03-07 21:14:49 +0100

    End video 58

--- Project/index10/app.js
+++ Project/index10/app.js
@@ -3,7 +3,8 @@ new Vue({
     data: {
         playerHealth: 100,
         monsterHealth: 100,
-        isGameRunning: false
+        isGameRunning: false,
+        turns: []
     },
     methods: {
         startGame: function () {
@@ -12,10 +13,20 @@ new Vue({
             this.monsterHealth = 100;
         },
         attack: function () {
-            this.monsterHealth -= this.calculcateDamage(3, 10);
+            var damage = this.calculcateDamage(3, 10);
+            this.monsterHealth -= damage;
+            this.turns.unshift({
+                isPlayer: true,
+                text: `Player hits Monster for ${damage}`
+            });
             if (this.checkWin())
                 return;
-            this.playerHealth -= this.calculcateDamage(5, 12);
+            damage = this.calculcateDamage(5, 12);
+            this.playerHealth -= damage;
+            this.turns.unshift({
+                isPlayer: false,
+                text: `Monster hits Player for ${damage}`
+            });
             this.checkWin()
         },
         specialAttack: function () {
