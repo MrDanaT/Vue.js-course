dd8e3fd2a2a8c655e9f7d7eb728313ec479cc970
Dana Tabatabaie Irani <dana.tabatabaieirani@gmail.com>
2020-03-07 18:39:10 +0100

    End video 54

--- Project/index10/app.js
+++ Project/index10/app.js
@@ -1,8 +1,49 @@
 new Vue({
     el: '#app',
-    data:{
+    data: {
         playerHealth: 100,
         monsterHealth: 100,
         isGameRunning: false
+    },
+    methods: {
+        startGame: function () {
+            this.isGameRunning = true;
+            this.playerHealth = 100;
+            this.monsterHealth = 100;
+        },
+        attack: function () {
+            var max = 10;
+            var min = 3;
+            var dmg = Math.max(Math.floor((Math.random() * max)) + 1, min);
+            this.monsterHealth -= dmg;
+
+            if (this.monsterHealth <= 0) {
+                alert('You won!');
+                this.isGameRunning = false;
+                return;
+            }
+
+            max = 12;
+            min = 5;
+            var dmg = Math.max(Math.floor((Math.random() * max)) + 1, min);
+            this.playerHealth -= dmg;
+
+
+            if (this.playerHealth <= 0) {
+                alert('You lost!');
+                this.isGameRunning = false;
+                return;
+            }
+        },
+        specialAttack: function () {
+
+        },
+        heal: function () {
+
+        },
+        giveUp: function () {
+            this.isGameRunning = false;
+        }
     }
+
 })
