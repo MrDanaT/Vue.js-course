4b61b8fd2313732a1e93a0f185b4d5c0126ede4d
Dana Tabatabaie Irani <dana.tabatabaieirani@gmail.com>
2020-03-24 01:41:17 +0100

    Einde video 101

--- Assignments/Exercise 6/src/App.vue
+++ Assignments/Exercise 6/src/App.vue
@@ -12,10 +12,10 @@
 </template>
 
 <script>
-import Header from './Header.vue';
-import Footer from './Footer.vue';
-import ServerDetails from './ServerDetails.vue';
-import Servers from './Servers.vue';
+import Header from './components/Shared/Header.vue';
+import Footer from './components/Shared/Footer.vue';
+import ServerDetails from './components/Server/ServerDetails.vue';
+import Servers from './components/Server/Servers.vue';
 
 export default {
   components: {
