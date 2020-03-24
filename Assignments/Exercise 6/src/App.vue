210659f1e6e06ea7de98d1fd959a729afe579cf2
Dana Tabatabaie Irani <dana.tabatabaieirani@gmail.com>
2020-03-24 01:22:18 +0100

    Finish assignment 6

---
+++ Assignments/Exercise 6/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class='container'>
+    <my-header></my-header>
+    <hr />
+    <div class='row'>
+      <my-servers></my-servers>
+      <my-server-details></my-server-details>
+    </div>
+    <hr />
+    <my-footer></my-footer>
+  </div>
+</template>
+
+<script>
+import Header from './Header.vue';
+import Footer from './Footer.vue';
+import ServerDetails from './ServerDetails.vue';
+import Servers from './Servers.vue';
+
+export default {
+  components: {
+    'my-footer': Footer,
+    'my-header': Header,
+    'my-server-details': ServerDetails,
+    'my-servers': Servers
+  }
+};
+</script>
+
+<style>
+</style>
