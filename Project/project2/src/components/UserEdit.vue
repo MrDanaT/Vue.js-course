3cb5da158da1bc3ac68c8b2427f63476ac5ba6ce
Dana Tabatabaie Irani <dana.tabatabaieirani@gmail.com>
2020-03-29 19:46:54 +0200

    End video 117
    Event Bus with method

--- Project/project2/src/components/UserEdit.vue
+++ Project/project2/src/components/UserEdit.vue
@@ -18,7 +18,8 @@ export default {
     editAge() {
       this.userAge = 30;
       //   this.$emit("ageWasEdited", this.userAge);
-      eventBus.$emit("ageWasEdited", this.userAge);
+      //   eventBus.$emit("ageWasEdited", this.userAge);
+      eventBus.changeAge(this.userAge);
     }
   }
 };
