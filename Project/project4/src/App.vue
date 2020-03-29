68aaef1438aaec50a3ae86e54a75bf4d00742b9d
Dana Tabatabaie Irani <dana.tabatabaieirani@gmail.com>
2020-03-30 01:29:37 +0200

    End video 140

---
+++ Project/project4/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container"></div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      quotes: ["First Quote"],
+      maxQuotes: 10
+    };
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ Project/project4/src/components/QuoteGrid.vue
@@ -0,0 +1,18 @@
+<template>
+<div class="row">
+
+</div>
+</template>
+
+<script>
+import Quote from "./Grid.vue";
+
+export default {
+    props:{
+        quotes: Array
+    }
+};
+</script>
+
+<style>
+</style>
