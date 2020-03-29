e96d6f3ee5b378518a4079a08f162751826669b1
Dana Tabatabaie Irani <dana.tabatabaieirani@gmail.com>
2020-03-29 22:57:57 +0200

    End video 125

--- Project/Section Code (Start)/src/App.vue
+++ Project/Section Code (Start)/src/App.vue
@@ -1,23 +1,28 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12">
-               <app-quote>
-                   <h2>The Quote</h2>
-                   <p>A wonderful Quote</p>
-               </app-quote>
-            </div>
-        </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <app-quote>
+          <h2>{{quoteTitle}}</h2>
+          <p>A wonderful Quote</p>
+        </app-quote>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-import Quote from './components/Quote.vue';
-    export default {
-        components:{
-            'app-quote': Quote
-        }
-    }
+import Quote from "./components/Quote.vue";
+export default {
+  data() {
+    return {
+        quoteTitle: 'The Quote'
+    };
+  },
+  components: {
+    "app-quote": Quote
+  }
+};
 </script>
 
 <style>
