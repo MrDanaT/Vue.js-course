4a3c68ce4d3d43d15ae25f06d3d4609e1654f1e3
Dana Tabatabaie Irani <dana.tabatabaieirani@gmail.com>
2020-04-06 20:32:38 +0200

    End video 154

--- Project/project5/src/App.vue
+++ Project/project5/src/App.vue
@@ -32,10 +32,10 @@
         <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
           <div class="form-group">
             <label for="sendmail">
-              <input type="checkbox" id="sendmail" value="SendMail" /> Send Mail
+              <input id="sendmail" type="checkbox" v-model="sendMail" value="SendMail"/> Send Mail
             </label>
             <label for="sendInfomail">
-              <input type="checkbox" id="sendInfomail" value="SendInfoMail" /> Send Infomail
+              <input id="sendInfomail" type="checkbox" v-model="sendMail" value="SendInfoMail"/> Send Infomail
             </label>
           </div>
         </div>
@@ -81,7 +81,9 @@
               <strong>Send Mail?</strong>
             </p>
             <ul>
-              <li></li>
+              <li v-for="item in sendMail">
+                {{item}}
+              </li>
             </ul>
             <p>Gender:</p>
             <p>Priority:</p>
@@ -98,12 +100,13 @@ export default {
 
     data(){
         return {
-            userData:{
-                email: '',
-                password: '',
-                age: 21
-            },
-            message: 'Some text'
+          userData: {
+            email: '',
+            password: '',
+            age: 21
+          },
+          message: 'Some text',
+          sendMail: []
         };
     }
 };
