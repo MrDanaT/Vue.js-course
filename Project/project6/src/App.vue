ab9ecaf26f87e236a942d229db1a224060597b8b
Dana Tabatabaie Irani <dana.tabatabaieirani@gmail.com>
2020-04-07 01:20:48 +0200

    End video 173

--- Project/project6/src/App.vue
+++ Project/project6/src/App.vue
@@ -12,7 +12,8 @@
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Custom Directives</h1>
                 <p v-highlight:background.delayed="'red'">Color this</p>
-                <p v-local-highlight:background.delayed.blink="'red'">Color this</p>
+                <p v-local-highlight:background.delayed.blink="{mainColor: 'red', secondColor: 'green', delay: 500}">
+                    Color this</p>
             </div>
         </div>
     </div>
@@ -30,8 +31,8 @@
                     }
 
                     if (binding.modifiers['blink']) {
-                        let mainColor = binding.value;
-                        let secondColor = 'blue';
+                        let mainColor = binding.value.mainColor;
+                        let secondColor = binding.value.secondColor;
                         let currentColor = mainColor;
                         setTimeout(() => {
                             setInterval(() => {
@@ -39,18 +40,16 @@
                                 if (binding.arg == 'background') {
                                     el.style.backgroundColor = currentColor;
                                 } else {
-
                                     el.style.color = currentColor;
                                 }
-                            }, 1000);
+                            }, binding.value.delay);
                         }, delay);
                     } else {
                         setTimeout(() => {
                             if (binding.arg == 'background') {
-                                el.style.backgroundColor = binding.value;
+                                el.style.backgroundColor = binding.value.mainColor;
                             } else {
-
-                                el.style.color = binding.value;
+                                el.style.color = binding.value.mainColor;
                             }
                         }, delay);
                     }
