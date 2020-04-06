dd439a1949150cb2fc001285a4bd31a134f7b064
Dana Tabatabaie Irani <dana.tabatabaieirani@gmail.com>
2020-04-06 21:05:34 +0200

    End video 159

--- Project/project5/src/App.vue
+++ Project/project5/src/App.vue
@@ -67,12 +67,12 @@
       <hr/>
       <div class="row">
         <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-          <button class="btn btn-primary">Submit!</button>
+          <button @click.prevent="submitted" class="btn btn-primary">Submit!</button>
         </div>
       </div>
     </form>
     <hr />
-    <div class="row">
+    <div class="row" v-if="isSubmitted">
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <div class="panel panel-default">
           <div class="panel-heading">
@@ -118,11 +118,17 @@
         gender: 'Male',
         selectedPrior: 'High',
         priorities: ['High', 'Medium', 'Low'],
-        dataSwitch: true
+        dataSwitch: true,
+        isSubmitted: false
       };
     },
     components: {
       'app-switch': Switch
+    },
+    methods: {
+      submitted() {
+        this.isSubmitted = true;
+      }
     }
   };
 </script>
