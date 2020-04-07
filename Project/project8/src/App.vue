4d7571aa6cf711f4ad0e6c6aad656ddf10b28d68
Dana Tabatabaie Irani <dana.tabatabaieirani@gmail.com>
2020-04-07 23:46:48 +0200

    End video 195

--- Project/project8/src/App.vue
+++ Project/project8/src/App.vue
@@ -6,7 +6,7 @@
                 <hr>
                 <button @click="show = !show" class="btn btn-primary">Show Alert</button>
                 <br><br>
-                <transition name="fade">
+                <transition name="slide">
                     <div class="alert alert-info" v-if="show">This is some info</div>
                 </transition>
             </div>
@@ -40,5 +40,39 @@
     .fade-leave-active {
         transition: opacity 1s;
         opacity: 0;
+    }
+
+    .slide-enter {
+
+    }
+
+    .slide-enter-active {
+        animation: slide-in 1s ease-out forwards;
+    }
+
+    .slide-leave {
+
+    }
+
+    .slide-leave-active {
+        animation: slide-out 1s ease-out forwards;
+    }
+
+    @keyframes slide-in {
+        from {
+            transform: translateY(20px);
+        }
+        to {
+            transform: translateY(0);
+        }
+    }
+
+    @keyframes slide-out {
+        from {
+            transform: translateY(0);
+        }
+        to {
+            transform: translateY(20px);
+        }
     }
 </style>
