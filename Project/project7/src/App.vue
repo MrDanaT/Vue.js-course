a8220eb7258699132412f615f70b6ae52b00644b
Dana Tabatabaie Irani <dana.tabatabaieirani@gmail.com>
2020-04-07 19:58:40 +0200

    End video 180

--- Project/project7/src/App.vue
+++ Project/project7/src/App.vue
@@ -4,6 +4,11 @@
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Filters & Mixins</h1>
                 <p>{{text | toUppercase | toLowercase}}</p>
+                <hr>
+                <input type="text" v-model="filterText">
+                <ul>
+                    <li v-for="fruit in filteredFruits">{{fruit}}</li>
+                </ul>
             </div>
         </div>
     </div>
@@ -13,12 +18,21 @@
     export default {
         data() {
             return {
-                text: 'Hello there!'
+                text: 'Hello there!',
+                fruits: ['Apple', 'Mango', 'Melon', 'Banana'],
+                filterText: ''
             }
         },
         filters: {
             toUppercase(value) {
                 return value.toString().toUpperCase();
+            }
+        },
+        computed: {
+            filteredFruits() {
+                return this.fruits.filter((el) => {
+                    return el.match(this.filterText);
+                })
             }
         }
     }
