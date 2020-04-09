8c58e60ae161179c15079ac893afd677e4300277
Dana Tabatabaie Irani <dana.tabatabaieirani@gmail.com>
2020-04-09 22:07:47 +0200

    End video 237

--- Project/project11/src/components/Header.vue
+++ Project/project11/src/components/Header.vue
@@ -1,11 +1,7 @@
 <template>
     <ul class="nav nav-pills">
-        <li role="presentation">
-            <router-link to="/">Home</router-link>
-        </li>
-        <li role="presentation">
-            <router-link to="/user">User</router-link>
-        </li>
+        <router-link active-class="active" exact tag="li" to="/"><a>Home</a></router-link>
+        <router-link active-class="active" tag="li" to="/user"><a>User</a></router-link>
     </ul>
 </template>
 
