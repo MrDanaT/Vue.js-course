8d966263af96249d1d8a898f211581cbaa1bb918
Dana Tabatabaie Irani <dana.tabatabaieirani@gmail.com>
2020-04-09 22:23:19 +0200

    End video 238

--- Project/project11/src/components/user/User.vue
+++ Project/project11/src/components/user/User.vue
@@ -1,4 +1,17 @@
 <template>
-    <h1>The User Page</h1>
+    <div>
+        <h1>The User Page</h1>
+        <hr>
+        <button @click="nagivateToHome" class="btn btn-primary">Go Home</button>
+    </div>
 
 </template>
+<script>
+    export default {
+        methods: {
+            nagivateToHome() {
+                this.$router.push({path: '/'});
+            }
+        }
+    }
+</script>
