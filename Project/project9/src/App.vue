f9b021f22a1f641c8fe510791476f08a4711b0c3
Dana Tabatabaie Irani <dana.tabatabaieirani@gmail.com>
2020-04-09 17:44:59 +0200

    End video 213

--- Project/project9/src/App.vue
+++ Project/project9/src/App.vue
@@ -3,14 +3,16 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1 class="text-center">The Super Quiz</h1>
+                <h2>Your current score: {{score}}</h2>
             </div>
         </div>
         <hr>
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
 
-                <component :is="mode" @answered="answered($event)" @confirmed="mode = 'app-question'"></component>
-
+                <transition mode="out-in" name="flip">
+                    <component :is="mode" @answered="answered($event)" @confirmed="mode = 'app-question'"></component>
+                </transition>
             </div>
         </div>
     </div>
@@ -23,13 +25,15 @@
     export default {
         data() {
             return {
-                mode: 'app-question'
+                mode: 'app-question',
+                score: 0
             }
         },
         methods: {
             answered(isCorrect) {
                 if (isCorrect) {
                     this.mode = 'app-answer';
+                    this.score += 10;
                 } else {
                     this.mode = 'app-question';
                     alert('Wrong, try again!');
@@ -42,4 +46,40 @@
         }
     }
 </script>
+
+<style>
+    .flip-enter {
+        /*transform: rotateY(0deg);*/
+    }
+
+    .flip-enter-active {
+        animation: flip-in 0.5s ease-out forwards;
+    }
+
+    .flip-leave {
+        /*transform: rotateY(0deg);*/
+    }
+
+    .flip-leave-active {
+        animation: flip-out 0.5s ease-out forwards;
+    }
+
+    @keyframes flip-out {
+        from {
+            transform: rotateY(0deg);
+        }
+        to {
+            transform: rotateY(90deg);
+        }
+    }
+
+    @keyframes flip-in {
+        from {
+            transform: rotateY(90deg);
+        }
+        to {
+            transform: rotateY(0deg);
+        }
+    }
+</style>
 
