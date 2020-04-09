21f3f6e635f22c68c83cdc5ccc6ababa7a018120
Dana Tabatabaie Irani <dana.tabatabaieirani@gmail.com>
2020-04-09 22:41:34 +0200

    End video 240

--- Project/project11/src/components/user/User.vue
+++ Project/project11/src/components/user/User.vue
@@ -2,12 +2,18 @@
     <div>
         <h1>The User Page</h1>
         <hr>
+        <p>Loaded ID: {{id}}</p>
         <button @click="nagivateToHome" class="btn btn-primary">Go Home</button>
     </div>
 
 </template>
 <script>
     export default {
+        data() {
+            return {
+                id: this.$route.params.id
+            }
+        },
         methods: {
             nagivateToHome() {
                 this.$router.push({path: '/'});
