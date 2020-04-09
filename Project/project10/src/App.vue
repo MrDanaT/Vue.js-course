e9ef48592547e4911ddd719366bdbbb61f5da6e2
Dana Tabatabaie Irani <dana.tabatabaieirani@gmail.com>
2020-04-09 18:49:43 +0200

    End video 227

--- Project/project10/src/App.vue
+++ Project/project10/src/App.vue
@@ -46,7 +46,8 @@
                 //         console.error(error);
                 //     });
 
-                this.resource.save({}, this.user);
+                // this.resource.save({}, this.user);
+                this.resource.saveAlt(this.user);
             },
             fetchData() {
                 this.$http
@@ -64,7 +65,10 @@
             }
         },
         created() {
-            this.resource = this.$resource('data.json');
+            const customActions = {
+                saveAlt: {method: 'POST', url: 'alternative.json'}
+            }
+            this.resource = this.$resource('data.json', {}, customActions);
         }
     }
 </script>
