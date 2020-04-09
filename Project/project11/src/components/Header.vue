877bdcf5777e2cacd13ec3a876938ad17595ff5d
Dana Tabatabaie Irani <dana.tabatabaieirani@gmail.com>
2020-04-09 21:29:47 +0200

    End video 236

---
+++ Project/project11/src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+    <ul class="nav nav-pills">
+        <li role="presentation">
+            <router-link to="/">Home</router-link>
+        </li>
+        <li role="presentation">
+            <router-link to="/user">User</router-link>
+        </li>
+    </ul>
+</template>
+
+<script>
+    export default {
+        name: "Header"
+    }
+</script>
+
+<style scoped>
+
+</style>
