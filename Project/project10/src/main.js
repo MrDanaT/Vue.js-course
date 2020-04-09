2aa45eaaace221e48af6c6a95cfdeb5b7f15b330
Dana Tabatabaie Irani <dana.tabatabaieirani@gmail.com>
2020-04-09 18:34:24 +0200

    End video 224

--- Project/project10/src/main.js
+++ Project/project10/src/main.js
@@ -5,8 +5,15 @@ import VueResource from 'vue-resource';
 Vue.use(VueResource);
 
 Vue.http.options.root = 'https://vue-course-7056e.firebaseio.com/data.json';
+Vue.http.interceptors.push((req, next) => {
+    console.log(req);
+    if (req.method == 'POST') {
+        req.method = 'PUT';
+    }
+    next();
+});
 
 new Vue({
-  el: '#app',
-  render: h => h(App)
+    el: '#app',
+    render: h => h(App)
 })
