8a3c0a742ced3fa161373766f11c3d8d187027ed
Dana Tabatabaie Irani <dana.tabatabaieirani@gmail.com>
2020-04-11 15:29:36 +0200

    End video 247

--- Project/project11/src/components/user/UserDetail.vue
+++ Project/project11/src/components/user/UserDetail.vue
@@ -3,7 +3,8 @@
         <h3>Some User Details</h3>
         <hr>
         <p>User loaded has ID: {{$route.params.id}}</p>
-        <router-link :to="{name: 'userEdit', params: {id: $route.params.id}}" class="btn btn-primary" tag="button">Edit
+        <router-link :to="{name: 'userEdit', params: {id: $route.params.id}, query: {locale: 'en', q: 100}}"
+                     class="btn btn-primary" tag="button">Edit
             User
         </router-link>
     </div>
