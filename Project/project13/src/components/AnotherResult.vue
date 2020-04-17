35e498a8fc53f6fa0856286ad2995818f089de32
Dana Tabatabaie Irani <dana.tabatabaieirani@gmail.com>
2020-04-17 19:44:18 +0200

    End video 267

--- Project/project13/src/components/AnotherResult.vue
+++ Project/project13/src/components/AnotherResult.vue
@@ -1,13 +1,18 @@
 <template>
-    <p>Counter is: {{ counter }}</p>
+    <div>
+        <p>Counter is: {{ doubleCounter }}</p>
+        <p>Numbers of Clicks: {{stringCounter}}</p>
+    </div>
 </template>
 
 <script>
+    import {mapGetters} from 'vuex';
+
     export default {
         computed: {
-            counter() {
-                return this.$store.getters.doubleCounter;
-            }
+            ...mapGetters([
+                'doubleCounter', 'stringCounter'
+            ])
         }
     }
 </script>
