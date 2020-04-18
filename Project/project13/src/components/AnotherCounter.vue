48a890e9907827137e5a4372fab9a26823989c92
Dana Tabatabaie Irani <dana.tabatabaieirani@gmail.com>
2020-04-18 22:09:17 +0200

    End video 269

---
+++ Project/project13/src/components/AnotherCounter.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <button @click="increment" class="btn btn-primary">Increment</button>
+        <button @click="decrement" class="btn btn-primary">Decrement</button>
+    </div>
+</template>
+
+<script>
+    import {mapMutations} from "vuex";
+
+    export default {
+        methods: {
+            ...mapMutations(['increment', 'decrement'])
+        }
+    }
+</script>
